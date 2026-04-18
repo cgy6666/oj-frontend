680d9f592bdd0a839c88723262c14090f46b69d2
cgy6666 <160098367+cgy6666@users.noreply.github.com>
2026-04-18 18:38:04 +0800

    Update problem.js

--- js/problem.js
+++ js/problem.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://oj-backend-theta.vercel.app/'; // 替换为你的Vercel域名
+const API_BASE = 'https://oj-backend-theta.vercel.app/api'; // 替换为你的Vercel域名
 let editor;
 
 function getProblemId() { return new URLSearchParams(window.location.search).get('id'); }
